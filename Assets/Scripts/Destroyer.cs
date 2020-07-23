50ee0437510bf2582f9c8a0db93ecaba86540bc7
brandontql <brandontql@gmail.com>
2020-07-23 23:30:48 +0800

    Fixed unlimited wall bug

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour {
 
-	// void OnTriggerEnter2D(Collider2D other){
-	// 	Destroy(other.gameObject);
+	void OnTriggerEnter2D(Collider2D other){
+		Destroy(other.gameObject);
 		
-	// }
+	}
 }
