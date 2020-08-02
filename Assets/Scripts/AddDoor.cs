0e2cd39a3933c659d92034b1b600d5cdd27694e3
brandontql <brandontql@gmail.com>
2020-08-02 14:04:06 +0800

    Added player enter room detection

---
+++ Assets/Scripts/AddDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddDoor : MonoBehaviour
+{
+    private RoomTemplates templates;
+
+	void Start(){
+
+		templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+		templates.doors.Add(this.gameObject);
+        gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/detectionDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectionDoor : MonoBehaviour
+{
+    public bool playerDetected;
+
+    private RoomTemplates templates;
+
+    void Start()
+    {
+        playerDetected = false;
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag ("Player")) 
+        {
+            Debug.Log("Player pass through");
+            playerDetected = true;
+            foreach (var obj in templates.doors)
+                obj.SetActive(true);
+        }
+
+    }
+}
