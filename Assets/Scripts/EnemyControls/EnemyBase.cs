de8b062ad655e77392abd29c2e586d64a3e217a4
Kenneth Sim <kensim28@hotmail.com>
2020-08-09 15:36:43 +0800

    Hungry ghost can move and shoot

--- Assets/Scripts/EnemyControls/EnemyBase.cs
+++ Assets/Scripts/EnemyControls/EnemyBase.cs
@@ -197,11 +197,6 @@ public class EnemyBase : MonoBehaviour
     }
 
 
-    public void RotateBody()
-    {
-        Vector3 direction = player.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-    }
+
 
 }
